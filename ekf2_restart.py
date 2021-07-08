82a9dd85f315884f770f0e1af704ab85e8cff2f4
deadln <glebuch2000@list.ru>
2021-07-08 13:33:23 +0300

    Added timeout parameter

--- ekf2_restart.py
+++ ekf2_restart.py
@@ -1,32 +1,17 @@
-import pyatspi
+import sys
 import time
 import keyboard
 
 
-def name_apps():
-    desktop = pyatspi.Registry.getDesktop(0)
-    print('there are %s running applications' % (desktop.childCount))
-    for app in desktop:
-        print(app.name)
-
-
-def show_windows():
-    desktop = pyatspi.Registry.getDesktop(0)
-    for app in desktop:
-        print("APP", app)
-        for window in app:
-            print("WINDOW",window)
-            for x in window:
-                print(x)
-
-
+timeout = float(sys.argv[1])
 t = time.time()
 while True:
     dt = time.time() - t
-    if dt > 30:
+    if dt > timeout:
         keyboard.write('ekf2 stop')
         keyboard.press_and_release('enter')
         time.sleep(2)
         keyboard.write('ekf2 start')
         keyboard.press_and_release('enter')
         t = time.time()
+        print("RESTART EKF2")
